8e084969a653c053781c4a99eb1ce0ca63a59cf3
davidto-samco <dto@samco-machinery.com>
2026-04-14 15:12:07 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        // console.log(s.split('').sort().join(''));
+        // console.log(t.split('').sort().join(''));
+        return s.split('').sort().join('') === t.split('').sort().join('');
+    }
+}
